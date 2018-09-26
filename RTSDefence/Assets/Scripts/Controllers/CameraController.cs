02fdb4ef74d606ff652835d93157078ee73a84bd
曹哲瑜 <>
2018-09-26 16:52:46 +0800

    Created personal test scenes

---
+++ RTSDefence/Assets/Scripts/Controllers/CameraController.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+
+namespace Controllers
+{
+    public class CameraController : MonoBehaviour
+    {
+        private Vector3 _lastMouse;
+        private bool _middleButtonDown;
+        private Camera _camera;
+        
+        private const float MovementChange = 10;
+        public Vector3 MinimumHeight = new Vector3(0, 1, 0);
+        public float MaximumHeight = 10f;
+
+        public void Start()
+        {
+            _camera = Camera.main;
+        }
+
+        public void FixedUpdate()
+        {
+            // Mouse middle click 
+            if (_middleButtonDown) 
+            {
+                var change = Input.mousePosition - _lastMouse;
+                transform.RotateAround(transform.position, Vector3.up, change.x);
+
+                _middleButtonDown = Input.GetMouseButton(2);
+                _lastMouse = Input.mousePosition;
+            }
+            else if (Input.GetMouseButton(2))
+            {
+                _middleButtonDown = true;
+                _lastMouse = Input.mousePosition;
+            }
+
+
+            // Mouse scroll
+            var scrollChange = Input.mouseScrollDelta.y * -2f;
+            // Clamp to top and bottom angle
+            if (transform.eulerAngles.x + scrollChange <= 80 && transform.eulerAngles.x + scrollChange >= 20)
+            {
+                transform.RotateAround(transform.position, transform.right, scrollChange);
+                _camera.orthographicSize += scrollChange / 6f;
+            }
+
+            // Keyboard commands
+            transform.position += GetBaseInput() * Time.deltaTime * MovementChange;
+        }
+
+        private Vector3 GetBaseInput()
+        {
+            var pVelocity = new Vector3();
+            if (Input.GetKey(KeyCode.D))
+            {
+                pVelocity += transform.right;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                pVelocity -= transform.right;
+            }
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                pVelocity += Vector3.Normalize(transform.forward - new Vector3(0, transform.forward.y, 0)) * 2;
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                pVelocity -= Vector3.Normalize(transform.forward - new Vector3(0, transform.forward.y, 0)) * 2;
+            }
+
+            return pVelocity;
+        }
+    }
+}
---
+++ RTSDefence/Assets/Scripts/Controllers/MouseController.cs
@@ -0,0 +1,30 @@
+﻿using Interface;
+using Navigation;
+using UnityEngine;
+
+namespace Controllers
+{
+    public class MouseController : MonoBehaviour
+    {
+        public IClickable FocusedItem { get; private set; }
+
+        public void SetFocus(IClickable click)
+        {
+            FocusedItem?.LostFocus();
+            FocusedItem = click;
+        }
+
+        private void Update()
+        {
+            // Right click
+            if (Input.GetMouseButtonDown(1))
+            {
+                Vector3 clickPos;
+                if (RaycastHelper.TryMouseRaycast(out clickPos))
+                {
+                    FocusedItem?.RightClick(clickPos);
+                }
+            }
+        }
+    }
+}
