0a072e0a8693203e7d3d88d62d105b54331136bf
Eric Chen <ericchen1248@gmail.com>
2018-09-26 23:57:31 +0800

    Start working on Buildables

---
+++ Assets/Scripts/Buildable Components/Buildable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Entity_Components;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Buildable_Components
+{
+    [RequireComponent(typeof(NavMeshObstacle))]
+    [RequireComponent(typeof(HealthComponent))]
+    public class Buildable : MonoBehaviour
+    {
+        public string Name { get; protected set; }
+
+        /// <summary>
+        /// A ghost model with no components on it to show user where the object will be placed
+        /// </summary>
+        public GameObject GhostModel;
+    
+        // TODO: need to set recipe
+        public Dictionary<object, int> RecipeDictionary { get; protected set; }
+
+        // Use this for initialization
+        protected virtual void Start ()
+        {
+            Name = "BaseBuildable";
+        }
+    }
+}
---
+++ Assets/Scripts/Controllers/BuildController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Buildable_Components;
+using UnityEngine;
+
+namespace Controllers
+{
+    public class BuildController : MonoBehaviour
+    {
+        public GameObject CurrentGameObject;
+        public GameObject CurrentGhostModel;
+        public bool IsBuilding { get; private set; }
+
+        // Use this for initialization
+        private void Start ()
+        {
+            enabled = false;
+        }
+	
+        // Update is called once per frame
+        private void Update ()
+        {
+            if (IsBuilding)
+            {
+                // If right clicked
+                if (Input.GetMouseButtonDown(1))
+                {
+                    DeselectBuild();
+                }
+                else if (Input.GetMouseButtonDown(0))
+                {
+                    Build();
+                }
+            } 
+            else
+            {
+                throw new BuildControllerException("Build controller is active when it shouldn't be");
+            }
+        }
+
+        public void EnableBuildMode(GameObject selectedGameObject)
+        {
+            CurrentGameObject = selectedGameObject;
+            GetComponent<MouseController>().enabled = false;
+
+            enabled = true;
+        }
+
+        private void DeselectBuild()
+        {
+            GetComponent<MouseController>().enabled = true;
+            IsBuilding = false;
+
+            CurrentGameObject = null;
+            CurrentGhostModel = null;
+
+            enabled = false;
+        }
+
+        private void CreateGhostModel()
+        {
+            Instantiate(CurrentGameObject.GetComponent<Buildable>().GhostModel);
+        }
+
+        private void Build()
+        {
+            Instantiate(CurrentGameObject);
+            DeselectBuild();
+        }
+        
+
+    }
+
+    [Serializable]
+    public class BuildControllerException : Exception
+    {
+        public BuildControllerException()
+        {
+        }
+
+        public BuildControllerException(string message) : base(message)
+        {
+        }
+
+        public BuildControllerException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected BuildControllerException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
