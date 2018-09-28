202273973cab92c7983e04636d692e9aa0c8bb92
Eric Chen <ericchen1248@gmail.com>
2018-09-28 23:38:01 +0800

    New version of Buildable class

--- Assets/Scripts/Buildable Components/Buildable.cs
+++ Assets/Scripts/Buildable Components/Buildable.cs
@@ -1,28 +1,10 @@
-﻿using System.Collections.Generic;
-using Entity_Components;
+﻿using Scriptable_Objects;
 using UnityEngine;
-using UnityEngine.AI;
 
 namespace Buildable_Components
 {
-    [RequireComponent(typeof(NavMeshObstacle))]
-    [RequireComponent(typeof(HealthComponent))]
     public class Buildable : MonoBehaviour
     {
-        public string Name { get; protected set; }
-
-        /// <summary>
-        /// A ghost model with no components on it to show user where the object will be placed
-        /// </summary>
-        public GameObject GhostModel;
-    
-        // TODO: need to set recipe
-        public Dictionary<object, int> RecipeDictionary { get; protected set; }
-
-        // Use this for initialization
-        protected virtual void Start ()
-        {
-            Name = "BaseBuildable";
-        }
+        public BuildData Data;
     }
 }
---
+++ Assets/Scripts/Scriptable Objects/BuildData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Scriptable_Objects
+{
+    [CreateAssetMenu]
+    public class BuildData : ScriptableObject
+    {
+        public string Name;
+        public GameObject GhostModel;
+        public RecipeItem[] Recipe;
+    }
+
+    [Serializable]
+    public struct RecipeItem
+    {
+        public GameObject GameObject;
+        public int Amount;
+    }
+}
