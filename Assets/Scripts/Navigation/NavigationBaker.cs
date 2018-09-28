6561df85d41aa762a49dd771308f986bfcae10f0
Eric Chen <ericchen1248@gmail.com>
2018-09-28 22:33:00 +0800

    Update NavigationBaker to v.3

--- Assets/Scripts/Navigation/NavigationBaker.cs
+++ Assets/Scripts/Navigation/NavigationBaker.cs
@@ -1,24 +1,51 @@
-﻿using UnityEngine;
+﻿using System.Threading;
+using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Serialization;
 
 namespace Navigation
 {
+    [DefaultExecutionOrder(-102)]
     public class NavigationBaker : MonoBehaviour
     {
         public static NavigationBaker Instance { get; private set; }
+        private NavMeshSurface[] _surface;
+        
+        private int _updateFrequency;
+        public int Mod = 5;
 
         public void Start ()
         {
             Instance = this;
-            Rebuild();
+            _surface = GetComponents<NavMeshSurface>();
+            Build();
         }
+
+        private void Update()
+        {
+            if (_updateFrequency++ % Mod == 0)
+            {
+                Rebuild();
+            }
+        }   
 
         public void Rebuild()
         {
-            foreach (var surface in GetComponentsInChildren<NavMeshSurface>())
+            foreach (var navMeshSurface in _surface)
             {
-                surface.BuildNavMesh();
+                navMeshSurface.UpdateNavMesh(navMeshSurface.navMeshData);
             }
         }
+        
+
+        public void Build()
+        {
+            foreach (var navMeshSurface in _surface)
+            {
+                navMeshSurface.BuildNavMesh();
+            }
+        }
+
+        
     }
 }
