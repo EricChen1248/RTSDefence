da898cf6d73a068b4eee0b5fbc66b5b9c4a3914f
Eric Chen <ericchen1248@gmail.com>
2018-10-04 19:09:14 +0800

    Add RigidyBody to Core

---
+++ Assets/Scripts/CoreComponent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Controllers;
+using UnityEngine;
+
+[DefaultExecutionOrder(-99)]
+public class CoreComponent : MonoBehaviour {
+
+	// Use this for initialization
+    private void Start ()
+	{
+	    CoreController.Instance.CoreGameObject = gameObject;
+	}
+	
+}
