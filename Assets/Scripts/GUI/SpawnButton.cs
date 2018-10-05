b8a49b25ec4d7ef4a52f1a21b993018e7d07e809
gimi65536 <gimi65536@hotmail.com>
2018-10-05 13:08:01 +0900

    測試用的spawn button
    增加一個新的按鈕生成enemy，以便測試

---
+++ Assets/Scripts/GUI/SpawnButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GUI{
+	public class SpawnButton : MonoBehaviour {
+		public GameObject SpawnObject;
+		public int PosXLowerBound = -10;
+		public int PosXUpperBound = 10;
+		public int PosZLowerBound = -10;
+		public int PosZUpperBound = 10;
+		public static System.Random rnd = new System.Random();
+		public void ClickEvent(){
+			//...
+			Vector3 v = new Vector3(rnd.Next(PosXLowerBound, PosXUpperBound), (float)0.75, rnd.Next(PosZLowerBound, PosZUpperBound));
+			Instantiate(SpawnObject, v, Quaternion.identity);
+		}
+	}
+}
