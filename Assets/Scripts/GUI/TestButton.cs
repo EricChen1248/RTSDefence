e7506bbdd136edd00e0aefa4e3ac51dc84daf52d
gimi65536 <gimi65536@hotmail.com>
2018-10-22 23:25:02 +0800

    修改TestButton的寫法
    ClickEvent只呼叫一個method，且請把想做的新事情寫成新的method，若要用到新的property也請自由添加。這是為了讓不同人測試時可以只增加或只更動ClickEvent的內容，方便git管理的相容性

--- Assets/Scripts/GUI/TestButton.cs
+++ Assets/Scripts/GUI/TestButton.cs
@@ -6,13 +6,20 @@ using Entity_Components;
 using Entity_Components.Ais;
 
 namespace GUI{
+	//write a method in TestButton
+	//and make ClickEvent call the method
+	//Free to add properties, but for compatibility, not to remove properties or methods
 	public class TestButton : MonoBehaviour {
 		public Vector3 V;
 		public void ClickEvent(){
-			//do everything you want to do.
+			//call one method
+			LeadAllGroup();
+		}
+		public void LeadAllGroup(){
 			foreach(Transform Group in CoreController.Instance.GroupsGameObject.GetComponent<GroupsComponent>().Children()){
 				Group.GetComponent<GroupAiBase>().LeadMemberTo(V);
 			}
 		}
+		//Add some methods if you want...
 	}
 }
