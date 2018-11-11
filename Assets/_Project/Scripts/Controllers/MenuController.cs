d1bc5f613e6db7b38055ae23577af2f4674c2cc4
Eric Chen <ericchen1248@gmail.com>
2018-11-11 20:27:04 +0800

    Add menu animation

---
+++ Assets/_Project/Scripts/Controllers/MenuController.cs
@@ -0,0 +1,29 @@
+﻿using Scripts.GUI;
+using UnityEngine;
+
+namespace Scripts.Controllers
+{
+    public class MenuController : MonoBehaviour {
+
+        public static MenuController Instance { get; private set; }
+        private MenuButton _currentMenu;
+
+        private void Start ()
+        {
+            Instance = this;
+        }
+
+        public void MenuClicked(MenuButton currentMenuButton)
+        {
+            if (_currentMenu != null) _currentMenu.Lower();
+            _currentMenu = currentMenuButton;
+        }
+
+        public void MenuLowered()
+        {
+            _currentMenu.Lower();
+            _currentMenu = null;
+        }
+
+    }
+}
