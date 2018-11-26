7c314bfe34fe22c011883c4d0e7bba711d5a1c3a
Eric Chen <ericchen1248@gmail.com>
2018-11-27 00:39:12 +0800

    New Debug Scripts Folder. New Draw Path Debug Script

---
+++ Assets/_Project/Scripts/DebugScripts/DrawPaths.cs
@@ -0,0 +1,30 @@
+﻿using Scripts.Entity_Components;
+using Scripts.Entity_Components.Friendlies;
+using UnityEngine;
+
+namespace Scripts.DebugScripts
+{
+    public class DrawPaths : MonoBehaviour
+    {
+
+        private bool _showing = true;
+        public void Click()
+        {
+
+            var objects = FindObjectsOfType<PlayerComponent>();
+            foreach (var playerComponent in objects)
+            {
+                if (!_showing)
+                {
+                    playerComponent.gameObject.AddComponent<PathDrawer>();
+                }
+                else
+                {
+                    Destroy(playerComponent.gameObject.GetComponent<PathDrawer>());
+                }
+            }
+
+            _showing = !_showing;
+        }
+    }
+}
