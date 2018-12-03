70ac83d44df9d240c5fb1fd6d3e16ed49ffaeff7
曹哲瑜 <>
2018-12-04 00:09:09 +0800

    Temp Push (Broken)

--- Assets/_Project/Scripts/Shooting/TurretAi.cs
+++ Assets/_Project/Scripts/Shooting/TurretAi.cs
@@ -7,23 +7,16 @@ public class TurretAi : MonoBehaviour
     public enum AiStates { NEAREST, FURTHEST, WEAKEST, STRONGEST };
 
     public AiStates aiState = AiStates.NEAREST;
-
-    TrackingSystem m_tracker;
-    ShootingSystem m_shooter;
-    RangeChecker m_range;
-
     // Use this for initialization
     void Start()
     {
-        m_tracker = GetComponent<TrackingSystem>();
-        m_shooter = GetComponent<ShootingSystem>();
-        m_range = GetComponent<RangeChecker>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!m_tracker || !m_shooter || !m_range)
+        if (!m_tracker)
             return;
 
         switch (aiState)
@@ -45,7 +38,7 @@ public class TurretAi : MonoBehaviour
 
     void TargetNearest()
     {
-        List<GameObject> validTargets = m_range.GetValidTargets();
+        List<GameObject> validTargets = null;//m_range.GetValidTargets();
 
         GameObject curTarget = null;
         float closestDist = 0.0f;
@@ -62,12 +55,11 @@ public class TurretAi : MonoBehaviour
         }
 
         m_tracker.SetTarget(curTarget);
-        m_shooter.SetTarget(curTarget);
     }
 
     void TargetFurthest()
     {
-        List<GameObject> validTargets = m_range.GetValidTargets();
+        List<GameObject> validTargets = null;//m_range.GetValidTargets();
 
         GameObject curTarget = null;
         float furthestDist = 0.0f;
@@ -84,50 +76,50 @@ public class TurretAi : MonoBehaviour
         }
 
         m_tracker.SetTarget(curTarget);
-        m_shooter.SetTarget(curTarget);
     }
 
     void TargetWeakest()
     {
-    //    List<GameObject> validTargets = m_range.GetValidTargets();
+        /*    List<GameObject> validTargets = m_range.GetValidTargets();
 
-    //    GameObject curTarget = null;
-    //    int lowestHealth = 0;
+        GameObject curTarget = null;
+        int lowestHealth = 0;
 
-    //    for (int i = 0; i < validTargets.Count; i++)
-    //    {
-    //        int maxHp = validTargets[i].GetComponent<Health>().maxHealth;
+        for (int i = 0; i < validTargets.Count; i++)
+        {
+            int maxHp = validTargets[i].GetComponent<Health>().maxHealth;
 
-    //        if (!curTarget || maxHp < lowestHealth)
-    //        {
-    //            lowestHealth = maxHp;
-    //            curTarget = validTargets[i];
-    //        }
-    //    }
+            if (!curTarget || maxHp < lowestHealth)
+            {
+                lowestHealth = maxHp;
+                curTarget = validTargets[i];
+            }
+        }
 
-    //    m_tracker.SetTarget(curTarget);
-    //    m_shooter.SetTarget(curTarget);
+        m_tracker.SetTarget(curTarget);
+        */
     }
 
     void TargetStrongest()
     {
-    //    List<GameObject> validTargets = m_range.GetValidTargets();
+        /*
+        List<GameObject> validTargets = m_range.GetValidTargets();
 
-    //    GameObject curTarget = null;
-    //    int highestHealth = 0;
+        GameObject curTarget = null;
+        int highestHealth = 0;
 
-    //    for (int i = 0; i < validTargets.Count; i++)
-    //    {
-    //        int maxHp = validTargets[i].GetComponent<Health>().maxHealth;
+        for (int i = 0; i < validTargets.Count; i++)
+        {
+            int maxHp = validTargets[i].GetComponent<Health>().maxHealth;
 
-    //        if (!curTarget || maxHp > highestHealth)
-    //        {
-    //            highestHealth = maxHp;
-    //            curTarget = validTargets[i];
-    //        }
-        //}
+            if (!curTarget || maxHp > highestHealth)
+            {
+                highestHealth = maxHp;
+                curTarget = validTargets[i];
+            }
+        }
 
-        //m_tracker.SetTarget(curTarget);
-        //m_shooter.SetTarget(curTarget);
+        m_tracker.SetTarget(curTarget);
+        */
     }
 }
