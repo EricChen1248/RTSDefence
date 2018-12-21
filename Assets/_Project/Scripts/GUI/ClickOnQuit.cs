4a7c4f84fbc319ca3b25a6355b12f46b99297e0f
ppppppa <43599917+ppppppa@users.noreply.github.com>
2018-12-21 21:53:10 +0800

    Change navmesh baking

--- Assets/_Project/Scripts/GUI/ClickOnQuit.cs
+++ Assets/_Project/Scripts/GUI/ClickOnQuit.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class ClickOnQuit : MonoBehaviour {
     public void Quit()
     {
+#if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
+#endif
         Application.Quit();
-
     }
 }
