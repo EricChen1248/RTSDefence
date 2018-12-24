69f65dd98eae11352e0b503f1700eecf02bdf6bb
gimi65536 <gimi65536@hotmail.com>
2018-12-24 20:03:03 +0900

    use OverlapSphere and linq to replace GetRangeHealth

--- Assets/_Project/Scripts/Entity Components/Status/ThunderComponent.cs
+++ Assets/_Project/Scripts/Entity Components/Status/ThunderComponent.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using Scripts.Entity_Components.Misc;
+using Scripts.Navigation;
 using UnityEngine;
 
 namespace Scripts.Entity_Components.Status
@@ -15,21 +16,15 @@ namespace Scripts.Entity_Components.Status
         public void Start () {
             var rnd = new System.Random();
             GetComponent<HealthComponent>()?.Damage(Damage);
-            foreach(var health in GetRangeHealth(transform, Range).Where(x => transform != x.transform).OrderBy(x => rnd.Next()).Take(NumberOfOtherVictim)){
+            var colliders = Physics.OverlapSphere(transform.position, Range, RaycastHelper.LayerMaskDictionary["Enemies"]);
+            var choose =
+                from c in colliders
+                where transform != c.transform && c.GetComponent<HealthComponent>() != null
+                select c.GetComponent<HealthComponent>();
+            foreach(var health in choose.OrderBy(x => rnd.Next()).Take(NumberOfOtherVictim)){
                 health.Damage(Damage);
             }
             Destroy(this);
-        }
-
-        public static List<HealthComponent> GetRangeHealth(Transform t, int Range){
-            var l = new List<HealthComponent>();
-            Vector3 v = t.position;
-            foreach(var health in FindObjectsOfType<HealthComponent>()){
-                if(Vector3.Distance(v, health.transform.position) <= Range){
-                    l.Add(health);
-                }
-            }
-            return l;
         }
     }
 }
